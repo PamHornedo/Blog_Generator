450eed1e63a332a4e3ca2be0b52fe11b9b7ca815
Pamela Hornedo <pamhornedo@gmail.com>
2025-10-11 00:22:51 -0400

    more styling

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const fs = require('fs').promises;
-const path = require('path');
-const marked = require('marked');
-const ejs = require('ejs');
-const { program } = require('commander');
+const fs = require("fs").promises;
+const path = require("path");
+const marked = require("marked");
+const ejs = require("ejs");
+const { program } = require("commander");
 
 async function generateSite() {
   // Source of the Markdown
-  const contentDir = './src/content';
+  const contentDir = "./src/content";
   // Deployable Docs Directory
-  const outputDir = './docs';
+  const outputDir = "./docs";
   // Template for the Page
-  const template = await fs.readFile('./src/templates/index.ejs', 'utf-8');
+  const template = await fs.readFile("./src/templates/index.ejs", "utf-8");
   // All the blog posts
   const files = await fs.readdir(contentDir);
 
   for (const file of files) {
-    if (file.endsWith('.md')) {
-      const markdown = await fs.readFile(path.join(contentDir, file), 'utf-8');
+    if (file.endsWith(".md")) {
+      const markdown = await fs.readFile(path.join(contentDir, file), "utf-8");
       const htmlContent = marked.parse(markdown);
       const output = await ejs.render(template, { content: htmlContent });
-      const outputFile = path.join(outputDir, file.replace('.md', '.html'));
+      const outputFile = path.join(outputDir, file.replace(".md", ".html"));
       await fs.writeFile(outputFile, output);
       console.log(`Generated ${outputFile}`);
     }
@@ -27,8 +27,8 @@ async function generateSite() {
 }
 
 program
-  .command('build')
-  .description('Generate the static site')
+  .command("build")
+  .description("Generate the static site")
   .action(generateSite);
 
 program.parse();
